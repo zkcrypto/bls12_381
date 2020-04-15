4d71da220ddd7aa396959e7b7c82f45cce255a96
CPerezz <c.perezbaro@gmail.com>
2020-04-15 19:54:04 +0200

    Implement Serde for Fp
    The serde implementation sets the `Fp` as 48 bytes,
    then encodes it and does the decoding and decompression
    on the deserialization.
    
    The deserialization will fail if the `Fp` was encoded
    incorrectly or the bytes are less than 48.
    
    Added tests for G1Affine roundtrip de/serialization.
    
    Task (1/2) to close #12

--- src/fp.rs
+++ src/fp.rs
@@ -4,6 +4,9 @@
 use core::convert::TryFrom;
 use core::fmt;
 use core::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+use serde::{
+    self, de::Visitor, ser::SerializeSeq, Deserialize, Deserializer, Serialize, Serializer,
+};
 
 use subtle::{Choice, ConditionallySelectable, ConstantTimeEq, CtOption};
 
@@ -48,6 +51,56 @@ impl PartialEq for Fp {
     #[inline]
     fn eq(&self, other: &Self) -> bool {
         self.ct_eq(other).unwrap_u8() == 1
+    }
+}
+
+
+impl Serialize for Fp {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut tup = serializer.serialize_seq(Some(48))?;
+        let fp_as_bytes = self.to_bytes();
+        for i in 0..48 {
+            tup.serialize_element(&fp_as_bytes[i])?;
+        }
+        tup.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for Fp {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct FpVisitor;
+
+        impl<'de> Visitor<'de> for FpVisitor {
+            type Value = Fp;
+
+            fn expecting(&self, formatter: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+                formatter.write_str("a prover key with valid powers per points")
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Fp, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                let mut bytes = [0u8; 48];
+                for i in 0..48 {
+                    bytes[i] = seq.next_element()?
+                        .ok_or(serde::de::Error::invalid_length(i, &"expected 48 bytes"))?;
+                }
+                let res = Fp::from_bytes(&bytes);
+                if res.is_some().unwrap_u8() == 1u8 {return Ok(res.unwrap())}
+                else {return Err(serde::de::Error::custom(
+                    &"fp was not canonically encoded"
+                ))}
+            }
+        }
+
+        deserializer.deserialize_seq(FpVisitor)
     }
 }
 
@@ -856,4 +909,14 @@ fn test_lexicographic_largest() {
         ])
         .lexicographically_largest()
     ));
+}
+
+#[test]
+fn fp_serde_roundtrip() {
+    use bincode;
+    let fp = Fp::one();
+    let ser = bincode::serialize(&fp).unwrap();
+    let deser: Fp = bincode::deserialize(&ser).unwrap();
+
+    assert_eq!(fp, deser);
 }
