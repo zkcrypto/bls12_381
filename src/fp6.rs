d42fd95e90c40056752da4caeb04761a227f4d44
CPerezz <c.perezbaro@gmail.com>
2020-04-18 13:29:31 +0200

    Implement Serde for `Fp6`
    The serde implementation sets the `Fp6` struct as a combination
    of 3 `Fp2` structs de/serializing them.
    
    The deserialization will fail if any of the `Fp2`s were encoded
    incorrectly.
    
    Added tests for roundtrip de/serialization.
    
    Implement Serde for Fp2
    
    We implement this serde support for `Fp6` since it will
    be necessary so later is easier for us to de/serialize
    `G2Prepared` which does not carry any `to_bytes` impl.

--- src/fp6.rs
+++ src/fp6.rs
@@ -58,6 +58,102 @@ impl fmt::Debug for Fp6 {
     }
 }
 
+#[cfg(feature = "serde")]
+use serde::{
+    self, de::Visitor, ser::SerializeStruct, Deserialize, Deserializer, Serialize, Serializer,
+};
+
+#[cfg(feature = "serde")]
+impl Serialize for Fp6 {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut fp2 = serializer.serialize_struct("struct Fp6", 3)?;
+        fp2.serialize_field("c0", &self.c0)?;
+        fp2.serialize_field("c1", &self.c1)?;
+        fp2.serialize_field("c2", &self.c2)?;
+        fp2.end()
+    }
+}
+
+#[cfg(feature = "serde")]
+impl<'de> Deserialize<'de> for Fp6 {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        enum Field {
+            C0,
+            C1,
+            C2,
+        };
+
+        impl<'de> Deserialize<'de> for Field {
+            fn deserialize<D>(deserializer: D) -> Result<Field, D::Error>
+            where
+                D: Deserializer<'de>,
+            {
+                struct FieldVisitor;
+
+                impl<'de> Visitor<'de> for FieldVisitor {
+                    type Value = Field;
+
+                    fn expecting(
+                        &self,
+                        formatter: &mut ::core::fmt::Formatter,
+                    ) -> ::core::fmt::Result {
+                        formatter.write_str("struct Fp6")
+                    }
+
+                    fn visit_str<E>(self, value: &str) -> Result<Field, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "c0" => Ok(Field::C0),
+                            "c1" => Ok(Field::C1),
+                            "c2" => Ok(Field::C2),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+
+                deserializer.deserialize_identifier(FieldVisitor)
+            }
+        }
+
+        struct Fp6Visitor;
+
+        impl<'de> Visitor<'de> for Fp6Visitor {
+            type Value = Fp6;
+
+            fn expecting(&self, formatter: &mut ::core::fmt::Formatter) -> ::core::fmt::Result {
+                formatter.write_str("struct Fp6")
+            }
+
+            fn visit_seq<V>(self, mut seq: V) -> Result<Fp6, V::Error>
+            where
+                V: serde::de::SeqAccess<'de>,
+            {
+                let c0 = seq
+                    .next_element()?
+                    .ok_or_else(|| serde::de::Error::invalid_length(0, &self))?;
+                let c1 = seq
+                    .next_element()?
+                    .ok_or_else(|| serde::de::Error::invalid_length(0, &self))?;
+                let c2 = seq
+                    .next_element()?
+                    .ok_or_else(|| serde::de::Error::invalid_length(0, &self))?;
+                Ok(Fp6 { c0, c1, c2 })
+            }
+        }
+
+        const FIELDS: &[&str] = &["c0", "c1", "c2"];
+        deserializer.deserialize_struct("Fp6", FIELDS, Fp6Visitor)
+    }
+}
+
 impl ConditionallySelectable for Fp6 {
     #[inline(always)]
     fn conditional_select(a: &Self, b: &Self, choice: Choice) -> Self {
@@ -504,4 +600,21 @@ fn test_arithmetic() {
         (&a * &b).invert().unwrap()
     );
     assert_eq!(&a.invert().unwrap() * &a, Fp6::one());
+}
+
+#[cfg(feature = "serde")]
+#[test]
+fn fp6_serde_roundtrip() {
+    use bincode;
+
+    let fp6 = Fp6 {
+        c0: Fp2::one(),
+        c1: Fp2::one(),
+        c2: Fp2::one(),
+    };
+
+    let ser = bincode::serialize(&fp6).unwrap();
+    let deser: Fp6 = bincode::deserialize(&ser).unwrap();
+
+    assert_eq!(fp6, deser);
 }
