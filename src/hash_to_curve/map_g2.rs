4f6edbc71bbb8434ff44f56faf52790620175b99
Andrew Whitehead <cywolf@gmail.com>
2021-03-29 00:05:29 -0700

    formatting adjustments
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- src/hash_to_curve/map_g2.rs
+++ src/hash_to_curve/map_g2.rs
@@ -17,59 +17,59 @@ use crate::{
 const ISO3_XNUM: [Fp2; 4] = [
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x47f671c71ce05e62,
-            0x06dd57071206393e,
-            0x7c80cd2af3fd71a2,
-            0x048103ea9e6cd062,
-            0xc54516acc8d037f6,
-            0x13808f550920ea41,
+            0x47f6_71c7_1ce0_5e62,
+            0x06dd_5707_1206_393e,
+            0x7c80_cd2a_f3fd_71a2,
+            0x0481_03ea_9e6c_d062,
+            0xc545_16ac_c8d0_37f6,
+            0x1380_8f55_0920_ea41,
         ]),
         c1: Fp::from_raw_unchecked([
-            0x47f671c71ce05e62,
-            0x06dd57071206393e,
-            0x7c80cd2af3fd71a2,
-            0x048103ea9e6cd062,
-            0xc54516acc8d037f6,
-            0x13808f550920ea41,
+            0x47f6_71c7_1ce0_5e62,
+            0x06dd_5707_1206_393e,
+            0x7c80_cd2a_f3fd_71a2,
+            0x0481_03ea_9e6c_d062,
+            0xc545_16ac_c8d0_37f6,
+            0x1380_8f55_0920_ea41,
         ]),
     },
     Fp2 {
         c0: Fp::zero(),
         c1: Fp::from_raw_unchecked([
-            0x5fe55555554c71d0,
-            0x873fffdd236aaaa3,
-            0x6a6b4619b26ef918,
-            0x21c2888408874945,
-            0x2836cda7028cabc5,
-            0x0ac73310a7fd5abd,
+            0x5fe5_5555_554c_71d0,
+            0x873f_ffdd_236a_aaa3,
+            0x6a6b_4619_b26e_f918,
+            0x21c2_8884_0887_4945,
+            0x2836_cda7_028c_abc5,
+            0x0ac7_3310_a7fd_5abd,
         ]),
     },
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x0a0c5555555971c3,
-            0xdb0c00101f9eaaae,
-            0xb1fb2f941d797997,
-            0xd3960742ef416e1c,
-            0xb70040e2c20556f4,
-            0x149d7861e581393b,
+            0x0a0c_5555_5559_71c3,
+            0xdb0c_0010_1f9e_aaae,
+            0xb1fb_2f94_1d79_7997,
+            0xd396_0742_ef41_6e1c,
+            0xb700_40e2_c205_56f4,
+            0x149d_7861_e581_393b,
         ]),
         c1: Fp::from_raw_unchecked([
-            0xaff2aaaaaaa638e8,
-            0x439fffee91b55551,
-            0xb535a30cd9377c8c,
-            0x90e144420443a4a2,
-            0x941b66d3814655e2,
-            0x0563998853fead5e,
+            0xaff2_aaaa_aaa6_38e8,
+            0x439f_ffee_91b5_5551,
+            0xb535_a30c_d937_7c8c,
+            0x90e1_4442_0443_a4a2,
+            0x941b_66d3_8146_55e2,
+            0x0563_9988_53fe_ad5e,
         ]),
     },
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x40aac71c71c725ed,
-            0x190955557a84e38e,
-            0xd817050a8f41abc3,
-            0xd86485d4c87f6fb1,
-            0x696eb479f885d059,
-            0x198e1a74328002d2,
+            0x40aa_c71c_71c7_25ed,
+            0x1909_5555_7a84_e38e,
+            0xd817_050a_8f41_abc3,
+            0xd864_85d4_c87f_6fb1,
+            0x696e_b479_f885_d059,
+            0x198e_1a74_3280_02d2,
         ]),
         c1: Fp::zero(),
     },
@@ -80,30 +80,30 @@ const ISO3_XDEN: [Fp2; 3] = [
     Fp2 {
         c0: Fp::zero(),
         c1: Fp::from_raw_unchecked([
-            0x1f3affffff13ab97,
-            0xf25bfc611da3ff3e,
-            0xca3757cb3819b208,
-            0x3e6427366f8cec18,
-            0x03977bc86095b089,
-            0x04f69db13f39a952,
+            0x1f3a_ffff_ff13_ab97,
+            0xf25b_fc61_1da3_ff3e,
+            0xca37_57cb_3819_b208,
+            0x3e64_2736_6f8c_ec18,
+            0x0397_7bc8_6095_b089,
+            0x04f6_9db1_3f39_a952,
         ]),
     },
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x447600000027552e,
-            0xdcb8009a43480020,
-            0x6f7ee9ce4a6e8b59,
-            0xb10330b7c0a95bc6,
-            0x6140b1fcfb1e54b7,
-            0x0381be097f0bb4e1,
+            0x4476_0000_0027_552e,
+            0xdcb8_009a_4348_0020,
+            0x6f7e_e9ce_4a6e_8b59,
+            0xb103_30b7_c0a9_5bc6,
+            0x6140_b1fc_fb1e_54b7,
+            0x0381_be09_7f0b_b4e1,
         ]),
         c1: Fp::from_raw_unchecked([
-            0x7588ffffffd8557d,
-            0x41f3ff646e0bffdf,
-            0xf7b1e8d2ac426aca,
-            0xb3741acd32dbb6f8,
-            0xe9daf5b9482d581f,
-            0x167f53e0ba7431b8,
+            0x7588_ffff_ffd8_557d,
+            0x41f3_ff64_6e0b_ffdf,
+            0xf7b1_e8d2_ac42_6aca,
+            0xb374_1acd_32db_b6f8,
+            0xe9da_f5b9_482d_581f,
+            0x167f_53e0_ba74_31b8,
         ]),
     },
     Fp2::one(),
@@ -113,59 +113,59 @@ const ISO3_XDEN: [Fp2; 3] = [
 const ISO3_YNUM: [Fp2; 4] = [
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x96d8f684bdfc77be,
-            0xb530e4f43b66d0e2,
-            0x184a88ff379652fd,
-            0x57cb23ecfae804e1,
-            0x0fd2e39eada3eba9,
-            0x08c8055e31c5d5c3,
+            0x96d8_f684_bdfc_77be,
+            0xb530_e4f4_3b66_d0e2,
+            0x184a_88ff_3796_52fd,
+            0x57cb_23ec_fae8_04e1,
+            0x0fd2_e39e_ada3_eba9,
+            0x08c8_055e_31c5_d5c3,
         ]),
         c1: Fp::from_raw_unchecked([
-            0x96d8f684bdfc77be,
-            0xb530e4f43b66d0e2,
-            0x184a88ff379652fd,
-            0x57cb23ecfae804e1,
-            0x0fd2e39eada3eba9,
-            0x08c8055e31c5d5c3,
+            0x96d8_f684_bdfc_77be,
+            0xb530_e4f4_3b66_d0e2,
+            0x184a_88ff_3796_52fd,
+            0x57cb_23ec_fae8_04e1,
+            0x0fd2_e39e_ada3_eba9,
+            0x08c8_055e_31c5_d5c3,
         ]),
     },
     Fp2 {
         c0: Fp::zero(),
         c1: Fp::from_raw_unchecked([
-            0xbf0a71c71c91b406,
-            0x4d6d55d28b7638fd,
-            0x9d82f98e5f205aee,
-            0xa27aa27b1d1a18d5,
-            0x02c3b2b2d2938e86,
-            0x0c7d13420b09807f,
+            0xbf0a_71c7_1c91_b406,
+            0x4d6d_55d2_8b76_38fd,
+            0x9d82_f98e_5f20_5aee,
+            0xa27a_a27b_1d1a_18d5,
+            0x02c3_b2b2_d293_8e86,
+            0x0c7d_1342_0b09_807f,
         ]),
     },
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0xd7f9555555531c74,
-            0x21cffff748daaaa8,
-            0x5a9ad1866c9bbe46,
-            0x4870a2210221d251,
-            0x4a0db369c0a32af1,
-            0x02b1ccc429ff56af,
+            0xd7f9_5555_5553_1c74,
+            0x21cf_fff7_48da_aaa8,
+            0x5a9a_d186_6c9b_be46,
+            0x4870_a221_0221_d251,
+            0x4a0d_b369_c0a3_2af1,
+            0x02b1_ccc4_29ff_56af,
         ]),
         c1: Fp::from_raw_unchecked([
-            0xe205aaaaaaac8e37,
-            0xfcdc000768795556,
-            0x0c96011a8a1537dd,
-            0x1c06a963f163406e,
-            0x010df44c82a881e6,
-            0x174f45260f808feb,
+            0xe205_aaaa_aaac_8e37,
+            0xfcdc_0007_6879_5556,
+            0x0c96_011a_8a15_37dd,
+            0x1c06_a963_f163_406e,
+            0x010d_f44c_82a8_81e6,
+            0x174f_4526_0f80_8feb,
         ]),
     },
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0xa470bda12f67f35c,
-            0xc0fe38e23327b425,
-            0xc9d3d0f2c6f0678d,
-            0x1c55c9935b5a982e,
-            0x27f6c0e2f0746764,
-            0x117c5e6e28aa9054,
+            0xa470_bda1_2f67_f35c,
+            0xc0fe_38e2_3327_b425,
+            0xc9d3_d0f2_c6f0_678d,
+            0x1c55_c993_5b5a_982e,
+            0x27f6_c0e2_f074_6764,
+            0x117c_5e6e_28aa_9054,
         ]),
         c1: Fp::zero(),
     },
@@ -175,49 +175,49 @@ const ISO3_YNUM: [Fp2; 4] = [
 const ISO3_YDEN: [Fp2; 4] = [
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x0162fffffa765adf,
-            0x8f7bea480083fb75,
-            0x561b3c2259e93611,
-            0x11e19fc1a9c875d5,
-            0xca713efc00367660,
-            0x03c6a03d41da1151,
+            0x0162_ffff_fa76_5adf,
+            0x8f7b_ea48_0083_fb75,
+            0x561b_3c22_59e9_3611,
+            0x11e1_9fc1_a9c8_75d5,
+            0xca71_3efc_0036_7660,
+            0x03c6_a03d_41da_1151,
         ]),
         c1: Fp::from_raw_unchecked([
-            0x0162fffffa765adf,
-            0x8f7bea480083fb75,
-            0x561b3c2259e93611,
-            0x11e19fc1a9c875d5,
-            0xca713efc00367660,
-            0x03c6a03d41da1151,
+            0x0162_ffff_fa76_5adf,
+            0x8f7b_ea48_0083_fb75,
+            0x561b_3c22_59e9_3611,
+            0x11e1_9fc1_a9c8_75d5,
+            0xca71_3efc_0036_7660,
+            0x03c6_a03d_41da_1151,
         ]),
     },
     Fp2 {
         c0: Fp::zero(),
         c1: Fp::from_raw_unchecked([
-            0x5db0fffffd3b02c5,
-            0xd713f52358ebfdba,
-            0x5ea60761a84d161a,
-            0xbb2c75a34ea6c44a,
-            0x0ac6735921c1119b,
-            0x0ee3d913bdacfbf6,
+            0x5db0_ffff_fd3b_02c5,
+            0xd713_f523_58eb_fdba,
+            0x5ea6_0761_a84d_161a,
+            0xbb2c_75a3_4ea6_c44a,
+            0x0ac6_7359_21c1_119b,
+            0x0ee3_d913_bdac_fbf6,
         ]),
     },
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x66b10000003affc5,
-            0xcb1400e764ec0030,
-            0xa73e5eb56fa5d106,
-            0x8984c913a0fe09a9,
-            0x11e10afb78ad7f13,
-            0x05429d0e3e918f52,
+            0x66b1_0000_003a_ffc5,
+            0xcb14_00e7_64ec_0030,
+            0xa73e_5eb5_6fa5_d106,
+            0x8984_c913_a0fe_09a9,
+            0x11e1_0afb_78ad_7f13,
+            0x0542_9d0e_3e91_8f52,
         ]),
         c1: Fp::from_raw_unchecked([
-            0x534dffffffc4aae6,
-            0x5397ff174c67ffcf,
-            0xbff273eb870b251d,
-            0xdaf2827152870915,
-            0x393a9cbaca9e2dc3,
-            0x14be74dbfaee5748,
+            0x534d_ffff_ffc4_aae6,
+            0x5397_ff17_4c67_ffcf,
+            0xbff2_73eb_870b_251d,
+            0xdaf2_8271_5287_0915,
+            0x393a_9cba_ca9e_2dc3,
+            0x14be_74db_faee_5748,
         ]),
     },
     Fp2::one(),
@@ -226,144 +226,144 @@ const ISO3_YDEN: [Fp2; 4] = [
 const SSWU_ELLP_A: Fp2 = Fp2 {
     c0: Fp::zero(),
     c1: Fp::from_raw_unchecked([
-        0xe53a000003135242,
-        0x01080c0fdef80285,
-        0xe7889edbe340f6bd,
-        0x0b51375126310601,
-        0x02d6985717c744ab,
-        0x1220b4e979ea5467,
+        0xe53a_0000_0313_5242,
+        0x0108_0c0f_def8_0285,
+        0xe788_9edb_e340_f6bd,
+        0x0b51_3751_2631_0601,
+        0x02d6_9857_17c7_44ab,
+        0x1220_b4e9_79ea_5467,
     ]),
 };
 
 const SSWU_ELLP_B: Fp2 = Fp2 {
     c0: Fp::from_raw_unchecked([
-        0x22ea00000cf89db2,
-        0x6ec832df71380aa4,
-        0x6e1b94403db5a66e,
-        0x75bf3c53a79473ba,
-        0x3dd3a569412c0a34,
-        0x125cdb5e74dc4fd1,
+        0x22ea_0000_0cf8_9db2,
+        0x6ec8_32df_7138_0aa4,
+        0x6e1b_9440_3db5_a66e,
+        0x75bf_3c53_a794_73ba,
+        0x3dd3_a569_412c_0a34,
+        0x125c_db5e_74dc_4fd1,
     ]),
     c1: Fp::from_raw_unchecked([
-        0x22ea00000cf89db2,
-        0x6ec832df71380aa4,
-        0x6e1b94403db5a66e,
-        0x75bf3c53a79473ba,
-        0x3dd3a569412c0a34,
-        0x125cdb5e74dc4fd1,
+        0x22ea_0000_0cf8_9db2,
+        0x6ec8_32df_7138_0aa4,
+        0x6e1b_9440_3db5_a66e,
+        0x75bf_3c53_a794_73ba,
+        0x3dd3_a569_412c_0a34,
+        0x125c_db5e_74dc_4fd1,
     ]),
 };
 
 const SSWU_XI: Fp2 = Fp2 {
     c0: Fp::from_raw_unchecked([
-        0x87ebfffffff9555c,
-        0x656fffe5da8ffffa,
-        0x0fd0749345d33ad2,
-        0xd951e663066576f4,
-        0xde291a3d41e980d3,
-        0x0815664c7dfe040d,
+        0x87eb_ffff_fff9_555c,
+        0x656f_ffe5_da8f_fffa,
+        0x0fd0_7493_45d3_3ad2,
+        0xd951_e663_0665_76f4,
+        0xde29_1a3d_41e9_80d3,
+        0x0815_664c_7dfe_040d,
     ]),
     c1: Fp::from_raw_unchecked([
-        0x43f5fffffffcaaae,
-        0x32b7fff2ed47fffd,
-        0x07e83a49a2e99d69,
-        0xeca8f3318332bb7a,
-        0xef148d1ea0f4c069,
-        0x040ab3263eff0206,
+        0x43f5_ffff_fffc_aaae,
+        0x32b7_fff2_ed47_fffd,
+        0x07e8_3a49_a2e9_9d69,
+        0xeca8_f331_8332_bb7a,
+        0xef14_8d1e_a0f4_c069,
+        0x040a_b326_3eff_0206,
     ]),
 };
 
 const SSWU_ETAS: [Fp2; 4] = [
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x05e514668ac736d2,
-            0x9089b4d6b84f3ea5,
-            0x603c384c224a8b32,
-            0xf3257909536afea6,
-            0x5c5cdbabae656d81,
-            0x075bfa0863c987e9,
+            0x05e5_1466_8ac7_36d2,
+            0x9089_b4d6_b84f_3ea5,
+            0x603c_384c_224a_8b32,
+            0xf325_7909_536a_fea6,
+            0x5c5c_dbab_ae65_6d81,
+            0x075b_fa08_63c9_87e9,
         ]),
         c1: Fp::from_raw_unchecked([
-            0x338d9bfe08087330,
-            0x7b8e48b2bd83cefe,
-            0x530dad5d306b5be7,
-            0x5a4d7e8e6c408b6d,
-            0x6258f7a6232cab9b,
-            0x0b985811cce14db5,
+            0x338d_9bfe_0808_7330,
+            0x7b8e_48b2_bd83_cefe,
+            0x530d_ad5d_306b_5be7,
+            0x5a4d_7e8e_6c40_8b6d,
+            0x6258_f7a6_232c_ab9b,
+            0x0b98_5811_cce1_4db5,
         ]),
     },
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x86716401f7f7377b,
-            0xa31db74bf3d03101,
-            0x14232543c6459a3c,
-            0x0a29ccf687448752,
-            0xe8c2b010201f013c,
-            0x0e68b9d86c9e98e4,
+            0x8671_6401_f7f7_377b,
+            0xa31d_b74b_f3d0_3101,
+            0x1423_2543_c645_9a3c,
+            0x0a29_ccf6_8744_8752,
+            0xe8c2_b010_201f_013c,
+            0x0e68_b9d8_6c9e_98e4,
         ]),
         c1: Fp::from_raw_unchecked([
-            0x05e514668ac736d2,
-            0x9089b4d6b84f3ea5,
-            0x603c384c224a8b32,
-            0xf3257909536afea6,
-            0x5c5cdbabae656d81,
-            0x075bfa0863c987e9,
+            0x05e5_1466_8ac7_36d2,
+            0x9089_b4d6_b84f_3ea5,
+            0x603c_384c_224a_8b32,
+            0xf325_7909_536a_fea6,
+            0x5c5c_dbab_ae65_6d81,
+            0x075b_fa08_63c9_87e9,
         ]),
     },
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0x718fdad24ee1d90f,
-            0xa58c025bed8276af,
-            0x0c3a10230ab7976f,
-            0xf0c54df5c8f275e1,
-            0x4ec2478c28baf465,
-            0x1129373a90c508e6,
+            0x718f_dad2_4ee1_d90f,
+            0xa58c_025b_ed82_76af,
+            0x0c3a_1023_0ab7_976f,
+            0xf0c5_4df5_c8f2_75e1,
+            0x4ec2_478c_28ba_f465,
+            0x1129_373a_90c5_08e6,
         ]),
         c1: Fp::from_raw_unchecked([
-            0x019af5f980a3680c,
-            0x4ed7da0e66063afa,
-            0x600354723b5d9972,
-            0x8b2f958b20d09d72,
-            0x0474938f02d461db,
-            0x0dcf8b9e0684ab1c,
+            0x019a_f5f9_80a3_680c,
+            0x4ed7_da0e_6606_3afa,
+            0x6003_5472_3b5d_9972,
+            0x8b2f_958b_20d0_9d72,
+            0x0474_938f_02d4_61db,
+            0x0dcf_8b9e_0684_ab1c,
         ]),
     },
     Fp2 {
         c0: Fp::from_raw_unchecked([
-            0xb8640a067f5c429f,
-            0xcfd425f04b4dc505,
-            0x072d7e2ebb535cb1,
-            0xd947b5f9d2b4754d,
-            0x46a7142740774afb,
-            0x0c31864c32fb3b7e,
+            0xb864_0a06_7f5c_429f,
+            0xcfd4_25f0_4b4d_c505,
+            0x072d_7e2e_bb53_5cb1,
+            0xd947_b5f9_d2b4_754d,
+            0x46a7_1427_4077_4afb,
+            0x0c31_864c_32fb_3b7e,
         ]),
         c1: Fp::from_raw_unchecked([
-            0x718fdad24ee1d90f,
-            0xa58c025bed8276af,
-            0x0c3a10230ab7976f,
-            0xf0c54df5c8f275e1,
-            0x4ec2478c28baf465,
-            0x1129373a90c508e6,
+            0x718f_dad2_4ee1_d90f,
+            0xa58c_025b_ed82_76af,
+            0x0c3a_1023_0ab7_976f,
+            0xf0c5_4df5_c8f2_75e1,
+            0x4ec2_478c_28ba_f465,
+            0x1129_373a_90c5_08e6,
         ]),
     },
 ];
 
 const SSWU_RV1: Fp2 = Fp2 {
     c0: Fp::from_raw_unchecked([
-        0x7bcfa7a25aa30fda,
-        0xdc17dec12a927e7c,
-        0x2f088dd86b4ebef1,
-        0xd1ca2087da74d4a7,
-        0x2da2596696cebc1d,
-        0x0e2b7eedbbfd87d2,
+        0x7bcf_a7a2_5aa3_0fda,
+        0xdc17_dec1_2a92_7e7c,
+        0x2f08_8dd8_6b4e_bef1,
+        0xd1ca_2087_da74_d4a7,
+        0x2da2_5966_96ce_bc1d,
+        0x0e2b_7eed_bbfd_87d2,
     ]),
     c1: Fp::from_raw_unchecked([
-        0x7bcfa7a25aa30fda,
-        0xdc17dec12a927e7c,
-        0x2f088dd86b4ebef1,
-        0xd1ca2087da74d4a7,
-        0x2da2596696cebc1d,
-        0x0e2b7eedbbfd87d2,
+        0x7bcf_a7a2_5aa3_0fda,
+        0xdc17_dec1_2a92_7e7c,
+        0x2f08_8dd8_6b4e_bef1,
+        0xd1ca_2087_da74_d4a7,
+        0x2da2_5966_96ce_bc1d,
+        0x0e2b_7eed_bbfd_87d2,
     ]),
 };
 
@@ -439,7 +439,7 @@ fn map_to_curve_simple_ssw(u: &Fp2) -> G2Projective {
 
     let x_num = Fp2::conditional_select(&x0_num, &(x0_num * xi_usq), eta_found);
     // ensure sign of y and sign of u agree
-    y.conditional_negate(u.sgn0() ^ y.sgn0()); // fix sign of y
+    y.conditional_negate(u.sgn0() ^ y.sgn0());
 
     G2Projective {
         x: x_num,
@@ -452,11 +452,11 @@ fn map_to_curve_simple_ssw(u: &Fp2) -> G2Projective {
 fn iso_map(u: &G2Projective) -> G2Projective {
     const COEFFS: [&[Fp2]; 4] = [&ISO3_XNUM, &ISO3_XDEN, &ISO3_YNUM, &ISO3_YDEN];
 
-    // xnum, xden, ynum, yden
-    let mut mapvals = [Fp2::zero(); 4];
-
     // unpack input point
     let G2Projective { x, y, z } = *u;
+
+    // xnum, xden, ynum, yden
+    let mut mapvals = [Fp2::zero(); 4];
 
     // compute powers of z
     let zsq = z.square();
@@ -479,7 +479,6 @@ fn iso_map(u: &G2Projective) -> G2Projective {
     mapvals[2] *= y;
     mapvals[3] *= z;
 
-    // projective coordinates of resulting point
     G2Projective {
         x: mapvals[0] * mapvals[3], // xnum * yden,
         y: mapvals[2] * mapvals[1], // ynum * xden,
@@ -524,6 +523,7 @@ fn test_osswu_semirandom() {
     }
 }
 
+// test vectors from the draft 07 RFC
 #[test]
 fn test_encode_to_curve_07() {
     use crate::{
@@ -602,6 +602,7 @@ fn test_encode_to_curve_07() {
     }
 }
 
+// test vectors from the draft 10 RFC
 #[test]
 fn test_encode_to_curve_10() {
     use crate::{
@@ -691,6 +692,7 @@ fn test_encode_to_curve_10() {
     }
 }
 
+// test vectors from the draft 07 RFC
 #[test]
 fn test_hash_to_curve_07() {
     use crate::{
@@ -769,6 +771,7 @@ fn test_hash_to_curve_07() {
     }
 }
 
+// test vectors from the draft 10 RFC
 #[test]
 fn test_hash_to_curve_10() {
     use crate::{
